d5fd01fe6f602c6b10c9da74eff75e1958b063cc
Techdemo <fleuren.marcel@gmail.com>
2021-08-21 17:26:49 +0200

    small change

--- routes/health.api.js
+++ routes/health.api.js
@@ -3,7 +3,8 @@ module.exports = function (app){
     res.status(200).json({
       resolved: "succes",
       data: {
-        health: "Api in good health"
+        health: "Api in good health",
+        alive: true,
       }
     })
   })
